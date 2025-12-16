72bb5b88a6d49ec9c1f90e89bbb889d37f94a247
System administrator <root@nixos.localdomain>
2025-12-16 04:13:24 +0000

    setup

---
+++ configuration.nix
@@ -0,0 +1,70 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+    (import "${home-manager}/nixos")
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "sugar";
+
+  users.users.sugar.isNormalUser = true;
+  home-manager.users.sugar = import ./sugar.nix;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05";
+  
+  environment.systemPackages = with pkgs; [
+    gcc
+    nodejs
+    nodePackages.npm
+    nodePackages.typescript
+    go
+    gopls
+    
+    # lazyvim deps
+    neovim
+    tree-sitter
+  ];
+
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "last-working-dir" ];
+      theme = "robbyrussell";
+    };
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+  };
+
+  users.users.sugar.shell = pkgs.zsh;
+
+  # Automatic updating
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.dates = "weekly";
+
+  # Automatic cleanup
+  nix.gc.automatic = true;
+  nix.gc.dates = "daily";
+  nix.gc.options = "--delete-older-than 10d";
+  nix.settings.auto-optimise-store = true;
+}
+
---
+++ sugar.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }: {
+  home.username = "sugar";
+  home.homeDirectory = "/home/sugar";
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    zoxide
+    atuin
+    croc
+    ripgrep
+    fd
+    fzf
+    xclip
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Marcus Sanchez";
+    userEmail = "marcussanchez031@gmail.com";
+  };
+
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper = {
+      enable = true;
+    };
+  };
+}
+
